3379393af89b4f7e3623d20cd681f9162a44dcaa
DavidYeLuo <davidayeluo@gmail.com>
2022-12-31 12:43:21 -0800

    Add documentation for Timer.cs

--- Prototype/Assets/Scripts/Util/Timer.cs
+++ Prototype/Assets/Scripts/Util/Timer.cs
@@ -4,11 +4,22 @@ using UnityEngine.PlayerLoop;
 
 namespace Util
 {
+    /// <summary>
+    /// Dispatch an event when time is up.
+    /// Subscribers should subscribe to timeUpEvent.
+    /// To start the timer call SetTimer()
+    /// </summary>
     public class Timer
     {
         public delegate void timeUp();
         public event timeUp timeUpEvent;
 
+        /// <summary>
+        /// Starts the timer that will dispatch an event when time is up.
+        /// Note: This is a coroutine function so use StartCoroutine() to use.
+        /// </summary>
+        /// <param name="seconds"> time in seconds </param>
+        /// <returns>Type: IEnumerator</returns>
         public IEnumerator SetTimer(float seconds)
         {
             yield return new WaitForSeconds(seconds);
