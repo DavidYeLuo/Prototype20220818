e6ee5ac46ea464b3ee5cf66bf3273ecf4af6b780
DavidYeLuo <davidayeluo@gmail.com>
2022-12-31 19:36:35 -0800

    Add KeyListener.cs documentation.

--- Prototype/Assets/Scripts/GameSystem/KeyListener.cs
+++ Prototype/Assets/Scripts/GameSystem/KeyListener.cs
@@ -3,9 +3,15 @@ using UnityEngine.Events;
 
 namespace GameSystem
 {
+    /// <summary>
+    /// Unity Component that maps a specific keyboard key to a script function.
+    /// </summary>
     public class KeyListener : MonoBehaviour
     {
+        [Header("Key Option")]
         [SerializeField] private KeyCode key;
+        
+        [Header("Key Events")]
         [SerializeField] private UnityEvent keyDown;
         [SerializeField] private UnityEvent keyUp;
         [SerializeField] private UnityEvent keyHold;
