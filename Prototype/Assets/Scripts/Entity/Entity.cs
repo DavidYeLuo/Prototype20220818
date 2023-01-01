35a9b9878cce704178e9fd2fb44e4ca2c4b693d1
DavidYeLuo <davidayeluo@gmail.com>
2022-12-31 22:51:21 -0800

    Add Entity.cs documentation.

--- Prototype/Assets/Scripts/Entity/Entity.cs
+++ Prototype/Assets/Scripts/Entity/Entity.cs
@@ -1,18 +1,24 @@
 using System.Collections.Generic;
 using Drivers.Health;
-using Entity;
+using Player;
 using Player.Abilities;
 using UnityEngine;
 
-namespace Player
+namespace Entity
 {
-    /**
-     * Prototype code: Note this is a monolithic class.
-     * Purpose of this class is for showcase proposed ideas.
-     */
+    /// <summary>
+    /// Unity component that represents the entity. <br/>
+    /// This should be the primary way to interact to the entity. <br/>
+    /// </summary>
+    /// <cursor>
+    /// Uses a virtual cursor system that some abilities require. <br/>
+    /// This virtual is to separate cursor dependencies. <br/>
+    /// Meaning that we can have AI control our character if we want to <br/>
+    /// without needing to create a new class <br/>
+    /// </cursor>
     public class Entity : MonoBehaviour, IAbilityUser, IMove, ICursorPosition, IHealth
     {
-        [Header("Drivers")] 
+        [Header("Drivers/Components")] 
         [SerializeField] private Health health;
         [SerializeField] private CursorPos cursor;
         [SerializeField] private Movement movement;
@@ -26,42 +32,69 @@ namespace Player
             }
         }
 
-        /**
-         * Moves the character at base speed
-         */
+        /// <summary>
+        /// Moves the current entity at base speed.
+        /// </summary>
+        /// <param name="position">Move to</param>
         public void Move(Vector3 position)
         {
             movement.Move(position);
         }
         
-        /**
-         * Moves the character at a set speed
-         */
+        /// <summary>
+        /// Moves the current entity at a specific speed
+        /// </summary>
+        /// <param name="position">Move to</param>
+        /// <param name="speed">Custom speed</param>
         public void Move(Vector3 position, float speed)
         {
             movement.Move(position, speed);
         }
 
+        /// <summary>
+        /// Uses Entity's ability. <br/>
+        /// Some entity have multiple abilities,
+        /// so they are accessed through an index
+        /// Index starts from 1
+        /// </summary>
+        /// <param name="n">index</param>
         public void UseAbility(int n)
         {
             abilityList[n].PerformAbility();
         }
 
+        /// <summary>
+        /// Accessor for current cursor position
+        /// </summary>
+        /// <returns>cursor position</returns>
         public Vector3 GetCursorPosition()
         {
             return cursor.GetPosition();
         }
 
+        /// <summary>
+        /// Sets the cursor position for the entity <br/>
+        /// Some abilities require the player's cursor. <br/>
+        /// </summary>
+        /// <param name="position"></param>
         public void SetCursorPosition(Vector3 position)
         {
             cursor.SetPosition(position);
         }
 
+        /// <summary>
+        /// Adds a given amount to the current health
+        /// </summary>
+        /// <param name="amount">Heal amount</param>
         public void Heal(int amount)
         {
             health.IncreaseHealth(amount);
         }
 
+        /// <summary>
+        /// Subtracts a given amount to the current health
+        /// </summary>
+        /// <param name="amount">damage amount</param>
         public void TakeDamage(int amount)
         {
             health.DecreaseHealth(amount);
