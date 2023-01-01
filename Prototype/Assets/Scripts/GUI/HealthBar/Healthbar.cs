de6dc0c4b58de511d2e44baf2ce87e31e787dd58
DavidYeLuo <davidayeluo@gmail.com>
2022-12-31 19:16:38 -0800

    Add healthbar.cs documentation.

--- Prototype/Assets/Scripts/GUI/HealthBar/Healthbar.cs
+++ Prototype/Assets/Scripts/GUI/HealthBar/Healthbar.cs
@@ -4,12 +4,24 @@ using UnityEngine.UI;
 
 namespace GUI.HealthBar
 {
+    /// <summary>
+    /// Unity Component that modifies the Image component. <br/>
+    /// It listens to the entity's currentHealth event. <br/>
+    /// Because currentHealth is IntReference, it broadcasts health change events. <br/>
+    /// This allows this program to listen to the moment when currentHealth change
+    /// and display accordingly. <br/>
+    /// </summary>
     public class Healthbar : MonoBehaviour
     {
         [Header("Dependencies")]
         [Header("GUI")]
         [Tooltip("Insert current health image")]
+        // Unity Image contains an attribute called fillAmount that the script
+        // will use to display the entity health.
         [SerializeField] private Image image;
+        
+        // These are the data that the script will use to calculate
+        // the appropriate fillAmount
         [Header("ScriptableObjects")]
         [SerializeField] private IntReference currentHealth;
         [SerializeField] private IntReference maxHealth;
@@ -30,6 +42,12 @@ namespace GUI.HealthBar
             maxHealth.subscribers -= UpdateGUI;
         }
 
+        /// <summary>
+        /// Calculates the appropriate the image fillAmount. <br/>
+        /// </summary>
+        /// <formula>
+        /// fillAmount = currentHealth / maxHealth
+        /// </formula>
         public void UpdateGUI()
         {
             image.fillAmount = (float) currentHealth.Get() / maxHealth.Get();
