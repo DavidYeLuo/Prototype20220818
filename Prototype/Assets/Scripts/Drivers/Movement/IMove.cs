5a8f925d697a47b2736f470791eb91eecf2ab608
DavidYeLuo <davidayeluo@gmail.com>
2023-01-01 10:37:26 -0800

    Add IMove.cs documentationi

--- Prototype/Assets/Scripts/Drivers/Movement/IMove.cs
+++ Prototype/Assets/Scripts/Drivers/Movement/IMove.cs
@@ -2,9 +2,21 @@ using UnityEngine;
 
 namespace Player
 {
+    /// <summary>
+    /// Interface for objects/entities that can move
+    /// </summary>
     public interface IMove
     {
+        /// <summary>
+        /// Interface that moves the entity at a base speed
+        /// </summary>
+        /// <param name="position"></param>
         public void Move(Vector3 position);
+        /// <summary>
+        /// Interface that moves the entity at a set speed
+        /// </summary>
+        /// <param name="position"></param>
+        /// <param name="speed"></param>
         public void Move(Vector3 position, float speed);
     }
 }
