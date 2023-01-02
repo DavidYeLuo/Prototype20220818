1efc49d95609f652ab08cc2cfd25f39e877e4207
DavidYeLuo <davidayeluo@gmail.com>
2023-01-01 18:07:40 -0800

    Add ICursorPosition.cs documentation.

--- Prototype/Assets/Scripts/Drivers/Cursor/ICursorPosition.cs
+++ Prototype/Assets/Scripts/Drivers/Cursor/ICursorPosition.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 
 namespace Entity
 {
+    /// <summary>
+    /// Interface for objects/entities that have a cursor. <br/>
+    /// </summary>
+    /// <example>
+    /// Player's mouse position
+    /// </example>
     public interface ICursorPosition
     {
+        /// <summary>
+        /// Accessor for where the object's cursor is.
+        /// </summary>
+        /// <returns>Position of the cursor</returns>
         public Vector3 GetCursorPosition();
+        /// <summary>
+        /// Sets where the cursor is.
+        /// </summary>
+        /// <param name="position">Desired position</param>
         public void SetCursorPosition(Vector3 position);
     }
 }
